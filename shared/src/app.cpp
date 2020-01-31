ca9b350c6f3f25c7d4a63cfd725b4d222198b137
Bjorklund <fibjorklund@ea.com>
2020-01-31 13:29:57 +0100

    Fixed bug in App

--- shared/src/app.cpp
+++ shared/src/app.cpp
@@ -1,17 +1,48 @@
 #include "app.hpp"
 
+#include "RenderAPI/BsRenderWindow.h"
+
 namespace wind {
 
-App::App(const bs::String& title, u32 w, u32 h) 
-	:	m_title(title), m_width(w), m_height(h)
-{}
-
-void App::run() 
+App::App(const Info& info)
+	: m_title(info.title), m_width(info.width), m_height(info.height)
 {
 	using namespace bs;
-	Application::startUp(VideoMode(m_width, m_height), m_title, false);
+
+	START_UP_DESC appDesc;
+	appDesc.renderAPI = "bsfD3D11RenderAPI";
+	appDesc.renderer = "bsfRenderBeast";
+	appDesc.physics = "bsfPhysX";
+	appDesc.audio = "bsfOpenAudio";
+	appDesc.importers = { "bsfFBXImporter", "bsfFontImporter", "bsfFreeImgImporter" };
+	appDesc.primaryWindowDesc.title = m_title;
+	appDesc.primaryWindowDesc.hidden = true;
+	appDesc.primaryWindowDesc.allowResize = true;
+	appDesc.primaryWindowDesc.videoMode = VideoMode(m_width, m_height);
+	Application::startUp(appDesc);
+}
+
+void App::run()
+{
+	using namespace bs;
+
+	show();
 	Application::instance().runMainLoop();
 	Application::shutDown();
+}
+
+void App::hide()
+{
+	using namespace bs;
+	SPtr<RenderWindow> window = gApplication().getPrimaryWindow();
+	window->hide();
+}
+
+void App::show()
+{
+	using namespace bs;
+	SPtr<RenderWindow> window = gApplication().getPrimaryWindow();
+	window->show();
 }
 
 
--- shared/src/app.hpp
+++ shared/src/app.hpp
@@ -2,6 +2,7 @@
 
 #include "BsApplication.h"
 
+
 #include "common.hpp"
 
 namespace wind {
@@ -9,9 +10,25 @@ namespace wind {
 class App 
 {
 public:
-	App(const bs::String& title, u32 w, u32 h);
+	/* App information */
+	struct Info {
+		bs::String title;
+		u32 width;
+		u32 height;
+	};
 
+public:
+	/* Create application */
+	App(const Info& info);
+
+	/* Run application */
 	void run();
+
+	/* Hide application window */
+	void hide();
+
+	/* Show application window */
+	void show();
 
 private:
 	/* Application title */
