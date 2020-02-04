4f402aa8410e3c8824dc4a23a484eb22a316c68d
Filip Björklund <max.filip.bjorklund@gmail.com>
2020-02-04 11:11:18 +0100

    Added basic editor features

---
+++ editor/src/editor.cpp
@@ -0,0 +1,156 @@
+// MIT License
+//
+// Copyright (c) 2020 Filip Björklund, Christoffer Gustafsson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "editor.hpp"
+
+// ========================================================================== //
+// Headers
+// ========================================================================== //
+
+#include "Resources/BsResources.h"
+#include "Resources/BsResourceManifest.h"
+#include "Resources/BsBuiltinResources.h"
+#include "Importer/BsImporter.h"
+#include "Importer/BsTextureImportOptions.h"
+#include "Importer/BsMeshImportOptions.h"
+#include "Importer/BsTextureImportOptions.h"
+#include "Components/BsCSkybox.h"
+#include "Components/BsCCamera.h"
+#include "Components/BsCRenderable.h"
+#include "Scene/BsSceneObject.h"
+#include "Mesh/BsMesh.h"
+#include "Material/BsMaterial.h"
+#include "BsCameraFlyer.h"
+
+// ========================================================================== //
+// Editor Implementation
+// ========================================================================== //
+
+namespace wind
+{
+
+Editor::Editor() : m_app(App::Info{"Editor", WINDOW_WIDTH, WINDOW_HEIGHT})
+{
+    registerControls();
+    setupCamera();
+    setupScene();
+    setupGUI();
+}
+
+// -------------------------------------------------------------------------- //
+
+void Editor::run()
+{
+    m_app.run();
+}
+
+// -------------------------------------------------------------------------- //
+
+void Editor::setupCamera()
+{
+    using namespace bs;
+
+    const SPtr<RenderWindow> window = gApplication().getPrimaryWindow();
+    const RenderWindowProperties &windowProp = window->getProperties();
+
+    // Skybox
+    const Path skyboxAssetPath = "res/skybox/daytime.hdr.asset";
+    const HTexture skyboxTex = gResources().load<Texture>(skyboxAssetPath);
+    HSceneObject skybox = SceneObject::create("Skybox");
+    HSkybox skyboxComp = skybox->addComponent<CSkybox>();
+    skyboxComp->setTexture(skyboxTex);
+
+    // Camera
+    HSceneObject camera = SceneObject::create("Camera");
+    camera->setPosition(Vector3(.0f, 2.5f, -4.0f) * 0.65f);
+    camera->lookAt(Vector3(.0f, 1.5f, .0f));
+    HCamera cameraComp = camera->addComponent<CCamera>();
+    cameraComp->getViewport()->setTarget(window);
+    cameraComp->setNearClipDistance(0.005f);
+    cameraComp->setFarClipDistance(1000);
+    cameraComp->setAspectRatio(windowProp.width / f32(windowProp.height));
+    const SPtr<RenderSettings> renderSettings = cameraComp->getRenderSettings();
+    renderSettings->enableIndirectLighting = true;
+    cameraComp->setRenderSettings(renderSettings);
+
+    // Flying camera
+    const HCameraFlyer cameraFlyerComp = camera->addComponent<CameraFlyer>();
+}
+
+// -------------------------------------------------------------------------- //
+
+void Editor::setupScene()
+{
+    using namespace bs;
+
+    // Load textures
+    const HTexture texGrid = gResources().load<Texture>("res/textures/grid.png.asset");
+    const HTexture texGrid2 = gResources().load<Texture>("res/textures/grid_2.png.asset");
+
+    // Setup plane
+    const HShader shader = gBuiltinResources().getBuiltinShader(BuiltinShader::Standard);
+    HMaterial planeMat = Material::create(shader);
+    planeMat->setTexture("gAlbedoTex", texGrid);
+    planeMat->setVec2("gUVTile", Vector2::ONE * GROUND_PLANE_SCALE * 0.5f);
+    const HMesh planeMesh = gBuiltinResources().getMesh(BuiltinMesh::Quad);
+
+    HSceneObject plane = SceneObject::create("Plane");
+    plane->setScale(Vector3(GROUND_PLANE_SCALE, 1.0f, GROUND_PLANE_SCALE));
+    HRenderable planeRenderable = plane->addComponent<CRenderable>();
+    planeRenderable->setMesh(planeMesh);
+    planeRenderable->setMaterial(planeMat);
+}
+
+// -------------------------------------------------------------------------- //
+
+void Editor::setupGUI()
+{
+}
+
+// -------------------------------------------------------------------------- //
+
+void Editor::registerControls()
+{
+    using namespace bs;
+
+    auto inputConfig = gVirtualInput().getConfiguration();
+
+    // Camera controls for buttons
+    inputConfig->registerButton("Forward", BC_W);
+    inputConfig->registerButton("Back", BC_S);
+    inputConfig->registerButton("Left", BC_A);
+    inputConfig->registerButton("Right", BC_D);
+    inputConfig->registerButton("Forward", BC_UP);
+    inputConfig->registerButton("Back", BC_DOWN);
+    inputConfig->registerButton("Left", BC_LEFT);
+    inputConfig->registerButton("Right", BC_RIGHT);
+    inputConfig->registerButton("FastMove", BC_LSHIFT);
+    inputConfig->registerButton("RotateObj", BC_MOUSE_LEFT);
+    inputConfig->registerButton("RotateCam", BC_MOUSE_RIGHT);
+    inputConfig->registerButton("Up", BC_SPACE);
+
+    // Camera controls
+    inputConfig->registerAxis("Horizontal", VIRTUAL_AXIS_DESC((UINT32)InputAxis::MouseX));
+    inputConfig->registerAxis("Vertical", VIRTUAL_AXIS_DESC((UINT32)InputAxis::MouseY));
+}
+
+} // namespace wind
---
+++ editor/src/editor.hpp
@@ -0,0 +1,73 @@
+// MIT License
+//
+// Copyright (c) 2020 Filip Björklund, Christoffer Gustafsson
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+// ========================================================================== //
+// Headers
+// ========================================================================== //
+
+#include "app.hpp"
+
+// ========================================================================== //
+// Editor Declaration
+// ========================================================================== //
+
+namespace wind
+{
+
+/* Main Editor class */
+class Editor
+{
+  public:
+    Editor();
+
+    void run();
+
+  private:
+    /* Setup the camera */
+    void setupCamera();
+
+    /* Setup the editor scene */
+    void setupScene();
+
+    /* Setup the editor GUI */
+    void setupGUI();
+
+    /* Register controls */
+    void registerControls();
+
+  public:
+    /* Editor window width */
+    static constexpr u32 WINDOW_WIDTH = 1280;
+    /* Editor window height */
+    static constexpr u32 WINDOW_HEIGHT = 720;
+
+    /* Scale of the ground plane */
+    static constexpr f32 GROUND_PLANE_SCALE = 50.0f;
+
+  private:
+    /* Application handle */
+    App m_app;
+};
+
+} // namespace wind
