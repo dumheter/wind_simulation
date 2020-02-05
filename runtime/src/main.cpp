3e595a56747d931169487c3ecde66243a3aa5beb
Christoffer Gustafsson <christoffer.golf@gmail.com>
2020-02-05 15:00:36 +0100

    Added sample functions to test the library on another machine.

--- runtime/src/main.cpp
+++ runtime/src/main.cpp
@@ -1,7 +1,11 @@
 #include "app.hpp"
 #include "world.hpp"
+#include <steam/isteamnetworkingutils.h>
+#include <steam/steamnetworkingsockets.h>
 
 int main() {
+  SteamDatagramErrMsg errMsg;
+  GameNetworkingSockets_Init(nullptr, errMsg);
   wind::World world{wind::App::Info{"Demo", 1280, 720}};
   world.run();
   return 0;
