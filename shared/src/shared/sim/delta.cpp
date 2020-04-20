4a08f741c5609536214343d83a0992968e539537
Filip Björklund <max.filip.bjorklund@gmail.com>
2020-04-20 10:48:02 +0200

    Offset delta bake sampling

--- shared/src/shared/sim/delta.cpp
+++ shared/src/shared/sim/delta.cpp
@@ -58,7 +58,7 @@ void DeltaField::build(const HCSim &csim, const HCWind &cwind) {
     for (u32 j = 0; j < dim.height; j++) {
       for (u32 i = 0; i < dim.width; i++) {
         Vec3F vSim = sim->V().get(i + 1, j + 1, k + 1);
-        Vec3F bakeOff(0.0f, 0.0f, 0.0f);
+        Vec3F bakeOff = Vec3F::ONE * 0.5f;
         Vec3F vBake = cwind->getWindAtPoint((Vec3F(i, j, k) + bakeOff) *
                                             sim->getCellSize());
         const bool obs = sim->O().get(i, j, k);
